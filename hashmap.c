799f9d589acbae9fbd76c12aa5943f6eef217948
Aguacatto <163459926+Aguacatto@users.noreply.github.com>
2024-04-25 03:14:58 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -128,9 +128,17 @@ Pair * searchMap(HashMap * map,  char * key)
   }
 }
 
-Pair * firstMap(HashMap * map) {
-
-    return NULL;
+Pair * firstMap(HashMap * map)
+{
+  for(long i = 0; i < map->capacity; i++)
+  {
+    if(map->buckets[i] != NULL && map->buckets[i]->key != NULL)
+    {
+      map->current = i;
+      return map->buckets[i];
+    }
+  }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
